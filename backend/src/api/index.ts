ac1d0cb29f4fa7af94fdc30c53190940b13d1b4c
josekadeveloper <josekadeveloper@gmail.com>
2025-04-08 00:47:21 +0200

    fix: frontend vercel config has changed wip

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import { sql } from "@vercel/postgres";
 import bodyParser from "body-parser";
-import path from "path";
+// import path from "path";
 
 dotenv.config();
 const app = express();
@@ -11,29 +11,29 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../../../", "frontend/Home/", "home.tsx"));
-});
+// app.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "../../../", "frontend/Home/", "home.tsx"));
+// });
 
-app.get("/home", function (req, res) {
-  res.sendFile(path.join(__dirname, "..", "components", "home.htm"));
-});
+// app.get("/home", function (req, res) {
+//   res.sendFile(path.join(__dirname, "..", "components", "home.htm"));
+// });
 
-app.get("/uploadUser", function (req, res) {
-  res.sendFile(
-    path.join(__dirname, "..", "components", "user_upload_form.htm")
-  );
-});
+// app.get("/uploadUser", function (req, res) {
+//   res.sendFile(
+//     path.join(__dirname, "..", "components", "user_upload_form.htm")
+//   );
+// });
 
-app.post("/uploadSuccessful", urlencodedParser, async (req, res) => {
-  try {
-    await sql`INSERT INTO Users (Id, Name, Email) VALUES (${req.body.user_id}, ${req.body.name}, ${req.body.email});`;
-    res.status(200).send("<h1>User added successfully</h1>");
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error adding user");
-  }
-});
+// app.post("/uploadSuccessful", urlencodedParser, async (req, res) => {
+//   try {
+//     await sql`INSERT INTO Users (Id, Name, Email) VALUES (${req.body.user_id}, ${req.body.name}, ${req.body.email});`;
+//     res.status(200).send("<h1>User added successfully</h1>");
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Error adding user");
+//   }
+// });
 
 app.get("/api/users", async (req, res) => {
   try {
