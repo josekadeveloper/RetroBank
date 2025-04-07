677c957ef735ef4f6993e343e7de0cd29724b014
josekadeveloper <josekadeveloper@gmail.com>
2025-04-07 23:22:24 +0200

    feature: backend has changed

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,28 +1,113 @@
+import dotenv from "dotenv";
 import express from "express";
-import serverless from "serverless-http";
-import cors from "cors";
+import { sql } from "@vercel/postgres";
 import bodyParser from "body-parser";
+import path from "path";
 
-import loginRoutes from "../routes/login";
-import registerRoutes from "../routes/register";
-import historyRoutes from "../routes/history";
-import usersRoutes from "../routes/users";
-
+dotenv.config();
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://retro-bank-frontend.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.use(bodyParser.json());
+app.use(express.static("public"));
 
-app.use("/api/login", loginRoutes);
-app.use("/api/register", registerRoutes);
-app.use("/api/history", historyRoutes);
-app.use("/api/users", usersRoutes);
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../../../", "frontend/Home/", "home.tsx"));
+});
 
-export const handler = serverless(app);
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "components", "home.htm"));
+});
+
+app.get("/uploadUser", function (req, res) {
+  res.sendFile(
+    path.join(__dirname, "..", "components", "user_upload_form.htm")
+  );
+});
+
+app.post("/uploadSuccessful", urlencodedParser, async (req, res) => {
+  try {
+    await sql`INSERT INTO Users (Id, Name, Email) VALUES (${req.body.user_id}, ${req.body.name}, ${req.body.email});`;
+    res.status(200).send("<h1>User added successfully</h1>");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error adding user");
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await sql`SELECT * FROM Users;`;
+    if (users && users.rows.length > 0) {
+      let tableContent = users.rows
+        .map(
+          (user) =>
+            `<tr>
+                        <td>${user.id}</td>
+                        <td>${user.name}</td>
+                        <td>${user.email}</td>
+                    </tr>`
+        )
+        .join("");
+
+      res.status(200).send(`
+                <html>
+                    <head>
+                        <title>Users</title>
+                        <style>
+                            body {
+                                font-family: Arial, sans-serif;
+                            }
+                            table {
+                                width: 100%;
+                                border-collapse: collapse;
+                                margin-bottom: 15px;
+                            }
+                            th, td {
+                                border: 1px solid #ddd;
+                                padding: 8px;
+                                text-align: left;
+                            }
+                            th {
+                                background-color: #f2f2f2;
+                            }
+                            a {
+                                text-decoration: none;
+                                color: #0a16f7;
+                                margin: 15px;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <h1>Users</h1>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>User ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                ${tableContent}
+                            </tbody>
+                        </table>
+                        <div>
+                            <a href="/">Home</a>
+                            <a href="/uploadUser">Add User</a>
+                        </div>
+                    </body>
+                </html>
+            `);
+    } else {
+      res.status(404).send("Users not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving users");
+  }
+});
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
