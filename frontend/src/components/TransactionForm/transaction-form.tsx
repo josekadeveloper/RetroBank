bb7a736b3f76bd1c2026aaa66ff833682538bfe7
josekadeveloper <josekadeveloper@gmail.com>
2025-04-08 20:14:20 +0200

    fix: login flow wip

--- frontend/src/components/TransactionForm/transaction-form.tsx
+++ frontend/src/components/TransactionForm/transaction-form.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { getUsers, saveTransaction, updateBalance } from "../store/storage";
+
+import { getUsers, saveTransaction, updateBalance } from "../../store/storage";
 
 type Props = {
   readonly user: string;
