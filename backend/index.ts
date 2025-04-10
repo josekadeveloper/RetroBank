b70747d1ecf253ee521bbb192b472cd9e1b6132e
josekadeveloper <josekadeveloper@gmail.com>
2025-04-10 22:30:47 +0200

    feature: authentication token has removed

--- backend/index.ts
+++ backend/index.ts
@@ -138,7 +138,12 @@ app.post(
         return res.status(401).json({ message: "Invalid credentials" });
       }
 
-      return res.status(200).json({ message: "Login successful" });
+      return res
+        .status(200)
+        .json({
+          message: "Login successful",
+          token: "ogBJQmJuEqJelWILxKwIhBNJQppOmOBG",
+        });
     } catch (error) {
       return res.status(500).json({ message: "Internal server error" });
     }
