9c85bf9772afaaaf41bb3f274b68a4ecfe8ea713
josekadeveloper <josekadeveloper@gmail.com>
2025-04-15 09:39:17 +0200

    fix: authentication login has updated wip

--- backend/index.ts
+++ backend/index.ts
@@ -137,12 +137,12 @@ app.post(
 
       if (!isPasswordValid) {
         res.status(401).json({ message: "Invalid credentials" });
+      } else {
+        res.status(200).json({
+          message: "Login successful",
+          token: process.env.JWT_SECRET,
+        });
       }
-
-      res.status(200).json({
-        message: "Login successful",
-        token: process.env.JWT_SECRET,
-      });
     } catch (error) {
       res.status(500).json({ message: "Internal server error" });
     }
